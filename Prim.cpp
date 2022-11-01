ccc09d2aa6638e1b6812bae1f265c6dee4b2572b
silasblade <yumekoviolet@gmail.com>
2022-11-02 01:03:03 +0700

    Update

--- Prim.cpp
+++ Prim.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <limits.h>
 #include <conio.h>
+#include <Windows.h>
 using namespace std;
 
 struct graph //Khai báo cấu trúc đồ thị với số đỉnh và độ dài các cạnh
@@ -39,30 +40,30 @@ int minvex(int* vex, bool* vexcheck, graph u) //Tìm ra cạnh nhỏ nhất so v
 
 void prim(graph u)
 {
-    int vex[100]; //Khai báo tập các đỉnh của đồ thị
+    int vex[100]; //Khai báo tập các cạnh của các đỉnh hiện đang nối với cây khung mà mình đăng chọn
     bool vexcheck[100]; //Xác nhận xem đỉnh đã được chọn hay chưa
-    for(int i=0; i<u.sodinh; i++) //Gán giá trị vô hạn k
+    for(int i=0; i<u.sodinh; i++) //Vòng lặp ban đầu
     {
-        vex[i]=INT_MAX; //Gán giá trị vô hạn độ dài cạnh đỉnh nối với đỉnh bất kỳ của cây đang tạo, có nghĩa là đỉnh này chưa nối với cây
+        vex[i]=INT_MAX; //Gán giá trị vô hạn cho các cạnh đang nối với cây hiện tại đang tìm, có nghĩa là đỉnh này chưa nối với cây
         vexcheck[i]=false; //Xác định cạnh nối đỉnh này với cây là chưa tối ưu
     }
     vex[0]=0; //Khai báo khoảng cách từ đỉnh ban đầu với đỉnh ban đầu được chọn = 0 
 
-    int branch[100]; //Khai báo các đỉnh nối với các đỉnh trong cây
+    int branch[100]; //Khai báo các đỉnh, đỉnh cho biết đỉnh nào nối với cây bằng đỉnh nào
     branch[0]=0; //Đỉnh nối với đỉnh ban đầu là chính nó
 
 
     for(int i=1; i<u.sodinh; i++) //Vòng lặp chạy từ đỉnh 1 đến đỉnh cuối, số lần lặp tương ứng với số cạnh nối với đỉnh tìm ra
     {
-       int add=minvex(vex, vexcheck, u); //Thao tác gán cho đỉnh được chọn (có cạnh nối với cây ngắn nhất so với các đỉnh có cạnh đang nối với cây hiện tại)
+       int add=minvex(vex, vexcheck, u); //Add là đỉnh được chọn trước đó (có cạnh nối với cây ngắn nhất so với các đỉnh có cạnh đang nối với cây hiện tại)
        vexcheck[add]=true; //Xác nhận đỉnh được chọn
 
-        for(int v=0; v<u.sodinh; v++) //Vòng lặp chạy từ đỉnh 0 tới đỉnh cuối, nhằm chọn ra các cạnh ngắn nhất được nối với cây hiện tại
+        for(int v=0; v<u.sodinh; v++) //Vòng lặp chạy từ đỉnh 0 tới đỉnh cuối, cập nhật lại các cạnh nối đỉnh chưa chọn với cây cho tối ưu.
         {
             if(vex[v] > u.dd[add][v] && u.dd[add][v] && vexcheck[v]==false) //Điều kiện là nếu cạnh nối với đỉnh vừa chọn, cạnh đó tồn tại, đỉnh nối với đỉnh vừa chọn chưa bị chọn
             {
                 vex[v]=u.dd[add][v]; //Gán cho tập các cạnh nối với đỉnh trong cây của mỗi đỉnh bất kỳ giá trị nhỏ nhất mà nó nhận được
-                branch[v]=add; //Xác định rằng đỉnh nối với đỉnh đó bằng khoảng cách đó
+                branch[v]=add; //Xác định đỉnh nối với đỉnh đang nối với cây là đỉnh nào.
             }
         }
 
@@ -70,12 +71,18 @@ void prim(graph u)
          
     }
 
-    cout << endl;
-
     for(int i=1; i<u.sodinh; i++) // Vòng lặp xuất ra các đỉnh nối với nhau và trọng số của chúng
     {
         cout << branch[i] << "--" << i << " = " << vex[i] << endl;
     }
+
+    wcout << "Tong trong so cay khung: ";
+    int S=0;
+    for(int i=0; i<u.sodinh; i++)
+    {
+        S+=vex[i];
+    }
+    cout << S;
 
     
 }
@@ -85,5 +92,5 @@ int main()
    graph u;
    readgraph(u);
    prim(u);
-   getch();
+   system("pause");
 }
